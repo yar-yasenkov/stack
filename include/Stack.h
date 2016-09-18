6419ff3161b9a0bdc0ad1992b9e0e541e0f46a1c
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-09-18 16:17:23 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -21,7 +21,7 @@ private:
 };
 
 template <typename T>
-void new_copy(T & array_,const stack<T> & obj)
+void new_copy(T array_,const stack<T> & obj)
 {
 	array_ = new T[obj.array_size_];
 	std::copy(obj.array_,obj.array_+obj.count_,array_);
