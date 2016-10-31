c820a7d028c543dfdb62906bd0134ef4e5dac55e
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-10-31 11:56:13 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -291,7 +291,7 @@ auto stack<T>::top()const->T const &
 template <typename T>/*noexcept*/
 auto stack<T>::empty()const->bool 
 {
-	return(allocator_.empty() == 0);
+	return(allocator_.empty() == 1);
 }
 
 template<typename T>
