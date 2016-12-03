83d2425b5d7e26962c46fe2f9fa03267f44898c7
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-12-03 14:40:13 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -291,7 +291,6 @@ auto stack<T>::top()const->T const &
 template <typename T>/*noexcept*/
 auto stack<T>::empty()const->bool 
 {
-	std::lock_guard<std::mutex> locker(mtxstack);
 	return(allocator_.empty() == 1);
 }
 
