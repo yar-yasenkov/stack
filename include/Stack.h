cc112b902bdde8a36951e518e57d30b6756c64f8
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-12-04 12:23:02 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -136,6 +136,7 @@ auto allocator<T>::destroy(T *ptr)-> void
 	{
 		ptr->~T(); 
 		map_->reset(ptr - ptr_);
+		
 	}
 }
 
@@ -172,7 +173,7 @@ auto allocator<T>::destroy(T *first,T *last)-> void
 {
 	if(first>last)
 	throw std::logic_error("errror");
-	for (; first != last; ++first)
+	for (; first < last; ++first)
 	{
 		destroy(first);
 	}	
